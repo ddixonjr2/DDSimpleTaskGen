f565090bdd1b43aac92e2993a42f26f85a8df243
Dennis Dixon <ddixonjr@gmail.com>
2025-07-25 11:44:13 -0400

    Initial commit with a basic working version of the command line functionality using the OpenAI Agent API.

---
+++ openaiagt/taskgen_openaiagt.py
@@ -0,0 +1,54 @@
+from agents import Agent, Runner, trace, gen_trace_id
+from dotenv import load_dotenv
+from argparse import ArgumentParser, Namespace
+
+import asyncio
+
+DEFAULT_INSTRUCTIONS = '''
+Role: You are a deeply knowledgeable, experienced, 
+and encouraging strategic and tactical planner.
+Task: You must utilize your vast knowledge of 
+how objectives like this have been accomplished,  
+determine the most effective strategy, and build a 
+realistic set of step in alignment with that strategy.  
+Constraints:
+Avoid profane and disrespectful information.
+Avoid all illegal, unethical, and immoral guidance, 
+methods, and actions.
+Input: You will be given a description of the person's objective.
+Output: You must organize the response in the form of a bulleted list. 
+This list must be broken down in terms of the  milestones, tasks, and 
+steps required.  Additionally provide an assessment of the priority of each.
+Capabilities and Reminders:  You have a vast amount of information at your disposal.  
+Remember to use it effectively by focusing on the most proven and highest quality 
+guidance available to you.  Also glean as many clues from the request 
+to customize the response.
+'''
+
+async def gen_task_list(request: str, instructions: str) -> str:
+    task_list_agent = Agent(
+        name='Expert Project Planner and Task List Generator',
+        instructions=instructions
+    )
+    trace_id = gen_trace_id()
+    with trace('Agent API Task List Generator v1', trace_id=trace_id):
+        response = await Runner.run(task_list_agent, input=request)
+        return response.final_output
+    
+async def main():
+    load_dotenv()
+    parser = ArgumentParser()
+    parser.add_argument('--request')
+    parser.add_argument('--instructions', default=DEFAULT_INSTRUCTIONS)
+    args = parser.parse_args()
+    request = args.request or input('What is your objective? ')
+    if request != '':
+        instructions = args.instructions or DEFAULT_INSTRUCTIONS
+        response = await gen_task_list(request, instructions)
+        print(response)
+    else:
+        print('\nSorry I cannot help without a description of what you would like to accomplish.')
+        print('Please try again by telling me your goal using the "--request" argument or by entering it when prompted.')
+
+if __name__ == '__main__':
+    asyncio.run(main())
